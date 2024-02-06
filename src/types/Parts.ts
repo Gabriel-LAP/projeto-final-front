f4b6cef91d84ec1ac84b859984898ec317649ddb
GBR <gb.gl@hotmail.com>
2024-02-06 11:51:43 -0300

    Created part types

---
+++ src/types/Parts.ts
@@ -0,0 +1,10 @@
+export type Part = {
+    _id?: string,
+    name: string,
+    purchasePrice: number,
+    quantity:  number,
+    brand: string,
+    category: string,
+    createdAt?: Date,
+    updatedAt: Date
+}
