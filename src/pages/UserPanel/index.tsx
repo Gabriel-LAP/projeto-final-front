0bb2bb701f1435994acd18a0f3d8e8813c2478a1
GBR <gb.gl@hotmail.com>
2024-02-06 11:47:02 -0300

    Changed userPanel

--- src/pages/UserPanel/index.tsx
+++ src/pages/UserPanel/index.tsx
@@ -4,17 +4,19 @@ import { colors } from '../../colors';
 import Header from '../../components/Header';
 import { UlComtainer } from '../../components/Ul';
 import { LiContainer } from '../../components/Ul/Li';
-import { v4 } from 'uuid';
-// import OrderPanel from '../../components/OrderPanel';
+// import { v4 } from 'uuid';
 import { useEffect, useState } from 'react';
 import { useOrders } from '../../Hooks';
 import { Orders } from '../../types/Orders';
 import { Container } from '../../components/Container';
 import Modal from 'react-modal';
-import { InfoContainer } from '../../components/Info';
 import Button from '../../components/Button';
 import EditOrderPanel from '../../components/EditOrderPanel';
-// import { useFormOrderHook } from '../../Hooks/useFormOrders';
+import { red } from '@mui/material/colors';
+import { deleteOrder } from '../../services/userService';
+import CreateOrderPanel from './CreateOrderPanel';
+import { priceFormate } from '../../mascaras/mascaraPreco';
+import { useParts } from '../../Hooks/useParts';
 
 const UserPainel = styled.div`
   display: flex;
@@ -39,65 +41,91 @@ const UserPainel = styled.div`
 
 const modalStyle = {
   overlay: {
-    backgroundColor: `${colors.darckIce}`,
+    position: 'fixed',
+    top: 0,
+    left: 242,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(000, 000, 000, 0.6)',
   },
 
-  buttonWrapper: {
-    display: 'flex',
-    justifyContent: 'center',
-    width: '57rem',
-    gap: '4rem',
-  },
   content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
     border: '1px solid green',
     background: '#839cff',
     borderRadius: '20px',
-    padding: '20px',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
     gap: '4rem',
     paddingTop: '3rem',
+    height: '30rem',
+    width: '70rem',
   },
 };
 
 Modal.setAppElement('#root');
 
 const UserPanel = () => {
-  const {
-    orders,
-    priceFormate,
-    handleOrders,
-    selectedOrder,
-    setSelectedOrder,
-  } = useOrders();
+  const { orders, handleOrders, selectedOrder, setSelectedOrder } = useOrders();
+  const { handlePartById } = useParts();
 
-  //   const { modalContent, setModalContent, modalIsOpen, setIsOpen } =
-  //     useFormOrderHook();
-  // const { navigate } = useFormOrderHook();
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [modalContent, setModalContent] = useState(true);
+  const [createOrderPanelisOpen, setCreateOrderPanelisOpen] = useState(false);
 
-  const handleClick = (order: Orders) => {
+  const handleEditButton = (order: Orders) => {
     setSelectedOrder(order);
 
     setIsOpen(true);
+    handleOrders();
 
     // console.log(order);
   };
 
   useEffect(() => {
     handleOrders();
-  }, []);
+  }, [orders]);
 
-  const [modalIsOpen, setIsOpen] = useState(false);
-  const [modalContent, setModalContent] = useState(true);
-
-  const handleEditOrder = (fechaModal: boolean, conteudoModal: boolean) => {
-    setIsOpen(fechaModal);
+  const handleEditOrder = (conteudoModal: boolean) => {
     setModalContent(conteudoModal);
     handleOrders();
   };
-  const fecharModal = () => {
-    setIsOpen(false);
+
+  const handleDeleteButton = (order: Orders) => {
+    deleteOrder(order);
+    handleOrders();
+  };
+  const fecharModal = (data: boolean) => {
+    handleOrders();
+    return setIsOpen(data);
+  };
+
+  const dataMask = (data: Date) => {
+    const value = data.toString().split('T', 9);
+    const date = value[0];
+    const time = value[1].slice(0, 8);
+    // console.log(date, time);
+    return `${date} / ${time}`;
+  };
+
+  const handleP = (p) => {
+    return handlePartById(p);
+  };
+
+  //   console.log(orders);
+
+  const openCreateClientPanel = ({
+    fechaCreateOrderPanel,
+  }: {
+    fechaCreateOrderPanel: boolean;
+  }) => {
+    setCreateOrderPanelisOpen(fechaCreateOrderPanel);
   };
 
   return (
@@ -116,23 +144,72 @@ const UserPanel = () => {
         </div>
 
         <UlComtainer padding='.9rem 0 0 0'>
-          {orders.length > 0 &&
+          {!createOrderPanelisOpen && (
+            <Button
+              text={'Novo Serviço'}
+              backgroundcolor={colors.purple}
+              height='3rem'
+              width='9rem'
+              color={colors.ice}
+              margin='.7rem 58rem 0 0'
+              onClick={() => setCreateOrderPanelisOpen(true)}
+            />
+          )}
+          {createOrderPanelisOpen ? (
+            <CreateOrderPanel openCreateOrderPanel={openCreateClientPanel} />
+          ) : (
+            orders.length > 0 &&
             orders.map((order) => (
               <LiContainer
-                activebgcolor={colors.mint}
-                activecolor={colors.purple}
-                key={v4()}
-                // className={index === 0 ? 'first-li' : ''}
-                onClick={() => handleClick(order)}
-                display='grid'
+                display='flex'
+                height='13rem'
+                gridRowRepeate='4, 25%'
+                gap='0'
+                spanSpace={false}
+                key={order._id}
               >
-                <span>Cliente: {order.owner.name}</span>
-                <span>Aparelho: {order.device}</span>
-                <span>Preço: {priceFormate(order.price)}</span>
-                <span>Garentia: {order.guarantee}</span>
-                <span>Defeito: {order.faulty}</span>
+                <Container
+                  width='55rem'
+                  display='grid'
+                  height='160px'
+                  gridTemplateColumns='50% 50%'
+                  gridTemplateRows='repeat(4, 25%)'
+                  gap='0rem 0.8rem'
+                  alignItems='center'
+                >
+                  <span>Aparelho: {order.owner.name}</span>
+                  <span>Preço: {priceFormate(order.price)}</span>
+                  <span>Garentia: {order.guarantee}</span>
+                  <span>Defeito: {order.faulty}</span>
+                  <span>Peças: {order.parts}</span>
+                  <span>Data de Criação: {dataMask(order.creationDate)}</span>
+                </Container>
+                <Container
+                  display='grid'
+                  gridTemplateRows='50% 50%'
+                  height='9rem'
+                  alignItems='center'
+                >
+                  <Button
+                    height='2rem'
+                    text={'Editar'}
+                    backgroundcolor={colors.mint}
+                    onClick={() => handleEditButton(order)}
+                    color={red.A200}
+                    width='5rem'
+                  />
+                  <Button
+                    height='2rem'
+                    text={'Excluir'}
+                    backgroundcolor={red.A200}
+                    onClick={() => handleDeleteButton(order)}
+                    color={colors.mint}
+                    width='5rem'
+                  />
+                </Container>
               </LiContainer>
-            ))}
+            ))
+          )}
 
           <Modal
             isOpen={modalIsOpen}
@@ -140,50 +217,15 @@ const UserPanel = () => {
             contentLabel='Informações do'
             style={modalStyle}
           >
-            {modalContent ? (
+            {modalContent && (
               <>
-                <div className='buttonWrapper' style={modalStyle.buttonWrapper}>
-                  <Button
-                    type='button'
-                    onClick={fecharModal}
-                    text='Fechar'
-                    width='200px'
-                    height='40px'
-                    backgroundcolor={colors.purple}
-                    activebgcolor={colors.blue}
-                    color={colors.ice}
-                    boxshadow='0px 5px 10px rgba(0, 0, 0, 60)'
-                  />
-                  <Button
-                    type='button'
-                    text='Editar'
-                    onClick={() => setModalContent(false)}
-                    width='200px'
-                    height='40px'
-                    backgroundcolor={colors.purple}
-                    activebgcolor={colors.blue}
-                    color={colors.ice}
-                    boxshadow='0px 5px 10px rgba(0, 0, 0, 60)'
-                  />
-                </div>
-
                 {selectedOrder && (
-                  <InfoContainer height='20rem'>
-                    <span>Aparelho: {selectedOrder.device}</span>
-                    <span>Dono: {selectedOrder.owner.name}</span>
-                    <span>Defeito: {selectedOrder.faulty}</span>
-                    <span>Peça: {selectedOrder.parts}</span>
-                    <span>Preço: {selectedOrder.price}</span>
-                    <span>Garantia: {selectedOrder.guarantee}</span>
-                  </InfoContainer>
+                  <EditOrderPanel
+                    order={selectedOrder}
+                    handleEdit={handleEditOrder}
+                    fechaModal={fecharModal}
+                  />
                 )}
-              </>
-            ) : (
-              <>
-                <EditOrderPanel
-                  order={selectedOrder}
-                  fechaModal={handleEditOrder}
-                />
               </>
             )}
           </Modal>
