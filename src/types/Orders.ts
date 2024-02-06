ec243d723d0f8ab0985a3dabf71f16f2dad0d24f
GBR <gb.gl@hotmail.com>
2024-02-06 11:50:24 -0300

    Changed Orders types

--- src/types/Orders.ts
+++ src/types/Orders.ts
@@ -1,17 +1,36 @@
 export type Orders = {
-    id: string,
+    _id?: string
     device: string,
     owner: {
-        name: string,
-        id: string
+        _id: string,
+        name: string
     },
+    ownerName?: string,
     faulty:  string,
-    parts: string,
+    parts?: string,
     price: number,
     statusOrder:  string,
     creationDate?: Date,
     estimatedDate?: Date,
     completionDate?: Date, 
     guarantee: string,
+    brand: string,
+
+}
+
+export type NewOrder = {
+    _id?: string
+    device: string,
+    owner: string,
+    parts?: string,
+    price: number,
+    statusOrder:  string,
+    creationDate?: Date,
+    estimatedDate?: Date,
+    completionDate?: Date,
+    guarantee: string,
+    brand: string,
+    faulty:  string,
+
 
 }
